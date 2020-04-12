bc3908b6a725f5d934d19e9633146d53f58e23a5
Zachary Skalin <zskalin@me.com>
2020-04-11 23:09:34 -0400

    Updated Vuetify config

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,28 @@
+import '@mdi/font/css/materialdesignicons.css';
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+// import colors from 'vuetify/lib/util/colors';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    options: {
+      customProperties: true,
+    },
+    themes: {
+      light: {
+        primary: '#1976D2',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107',
+      },
+    },
+  },
+  icons: {
+    iconfont: 'mdi',
+  },
+});
