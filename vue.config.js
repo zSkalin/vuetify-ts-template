26fdb65eaced4dfea135ab35fe70c7f378e81e61
Zachary Skalin <zskalin@me.com>
2021-01-01 16:05:04 -0500

    Converted to var require, unable to use import on non-module

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-import { version } from './package.json';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const version = require('./package.json').version;
 process.env.VUE_APP_VERSION = version;
 
 module.exports = {
